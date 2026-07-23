bf2566de8676a1fd4a38cccd5c9f31adc9a962e0
RealStiv <durangostiven444@gmail.com>
2026-07-23 00:21:26 -0400

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 # Smm Panel Bot
 # Author: learningbots79 (https://github.com/learningbots79) 
 # Support: https://t.me/LearningBotsCommunity
-# Channel: https://t.me/
+# Channel: https://t.me/learning_bots
 # YouTube: https://youtube.com/@learning_bots
 # License: Open-source (keep credits, no resale)
 # ============================================================
@@ -14,24 +14,20 @@ from datetime import datetime, timedelta
 import db
 from handlers import all_handlers, tarea_sincronizacion_automatica, tareas_mantenimiento
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, INTERVALO_SINCRONIZACION
+from config import BOT_TOKEN, INTERVALO_SINCRONIZACION
 
 logging.basicConfig(level=logging.INFO)
 
 app = Client(
     "panel_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
 async def iniciar_tareas_en_segundo_plano():
     """Ejecuta procesos automáticos sin detener el bot"""
-    # Ejecuta sincronización apenas inicie
     print("⏳ Realizando sincronización inicial...")
     await tarea_sincronizacion_automatica()
     
-    # Bucle infinito de tareas
     while True:
         await tarea_sincronizacion_automatica()
         await tareas_mantenimiento()
